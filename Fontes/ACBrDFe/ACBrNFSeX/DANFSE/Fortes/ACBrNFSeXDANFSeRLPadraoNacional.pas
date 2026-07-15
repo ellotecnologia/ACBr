6bbe2d8e9bf44c957ea735147d487f66f719ac24
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-07-15 21:16:29 +0000

    15/07/2026 -- ACBrNFSeXDANFSeRLPadraoNacional -- [-] Corrigido o calculo do Valor Liquido mais o IBS/CBS.    Por: Innovae Brasil Engenharia
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@47225 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fortes/ACBrNFSeXDANFSeRLPadraoNacional.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fortes/ACBrNFSeXDANFSeRLPadraoNacional.pas
@@ -1031,7 +1031,7 @@ begin
   else
     rllValorTotalIBSCBS.Caption := '-';
 
-  LValor := fpNFSe.Servico.Valores.ValorServicos +
+  LValor := fpNFSe.Servico.Valores.ValorLiquidoNfse +
             fpNFSe.infNFSe.IBSCBS.totCIBS.gIBS.vIBSTot +
             fpNFSe.infNFSe.IBSCBS.totCIBS.gCBS.vCBS;
 
